8f6dacf1a43cd8593ad50abc87698143a52ef879
Fikri <fikryyeg@gmail.com>
2026-09-10 13:07:04 +0700

    disable product seeders for selective run

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,10 +10,10 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Truncate tabel products secara aman sebelum seeder running
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Product::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        // MATIKAN TRUNCATE PRODUK
+        // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // Product::truncate();
+        // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $this->call([
             StoreSeeder::class,
@@ -22,15 +22,14 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             AksesorisSeeder::class,
             PpobServerSeeder::class,
-            // 1. Buat Data Master Produk Terlebih Dahulu
-            PulsaProductSeeder::class,
-            VoucherProductSeeder::class,
-            PerdanaProductSeeder::class,
-            EwalletBankProductSeeder::class,
-            AksesorisProductSeeder::class,
-
-            // 2. Generasi Stok Fisik Cabang Berdasarkan Produk yang Sudah Ada
-            StoreProductStockSeeder::class,
+            
+            // MATIKAN SEMUA SEEDER PRODUK & STOK AGAR HARGA TIDAK TERTIMPA
+            // PulsaProductSeeder::class,
+            // VoucherProductSeeder::class,
+            // PerdanaProductSeeder::class,
+            // EwalletBankProductSeeder::class,
+            // AksesorisProductSeeder::class,
+            // StoreProductStockSeeder::class,
         ]);
     }
 }
